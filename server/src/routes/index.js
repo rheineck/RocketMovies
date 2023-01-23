373f1aa948f99505fe2b6ca11eb055d035282cc5
Raphael <raphaelheineck@gmail.com>
2023-01-23 15:54:19 -0300

    Back-End Finished

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express')
 
 
-const usersRoutes = require('./users.routes')
+const usersRouter = require('./users.routes')
+const notesRouter = require('./notes.routes')
+const tagsRouter = require('./tags.routes')
 
 const routes = Router()
 
-routes.use("/users", usersRoutes)
+routes.use("/users", usersRouter)
+routes.use("/notes", notesRouter)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes
